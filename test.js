a7a247a10d7e0bc6565b95d79a2c9f41115255ff
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-22 16:19:45 -0400

    Fix test cases

--- test.js
+++ test.js
@@ -2,19 +2,27 @@
 var assert = require('assert');
 var ghls = require('./');
 
-it('should download the file list from master', function () {
+it('should download the file list from master by default', function (done) {
     ghls('metacran/gh-ls', function (err, list) {
+	assert(!err);
 	var files = list.tree.map(function(x) { return x.path; });
-	assert(files.indexOf('package.json') >= 0)
-    })
-    ghls('metacran/gh-ls', { 'sha': 'master' }, function (err, list) {
-	var files = list.tree.map(function(x) { return x.path; });
-	assert(files.indexOf('package.json') >= 0)
+	assert(files.indexOf('package.json') >= 0);
+	done();
     })
 })
 
-it('should error on non-existing repos', function() {
+it('should use the branch if specified', function(done) {
+    ghls('metacran/gh-ls', { 'sha': 'master' }, function (err, list) {
+	assert(!err);
+	var files = list.tree.map(function(x) { return x.path; });
+	assert(files.indexOf('package.json') >= 0);
+	done();
+    })
+})
+
+it('should error on non-existing repos', function(done) {
     ghls('metacran/this-obviously-does-not-exist', function(err, list) {
 	assert(err);
+	done();
     })
 })
